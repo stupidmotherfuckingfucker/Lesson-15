74e7354e549e9ed2c43d875a9294207cdcd1dae7
kek <astanaevv@bk.ru>
2021-08-18 15:01:52 +0700

    Добавьте файлы проекта.

---
+++ Lesson 15.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+
+int main()
+{
+	int N;
+	std::cin >> N;
+	for (int i = 0; i < N; ++i)
+	{
+		if (i % 2 == 0) //Проверка на четность 
+		{
+			std::cout << i << " ";  
+		}
+		else
+			std::cout << i << " "; 
+	}
+}
